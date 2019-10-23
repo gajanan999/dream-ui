4f829c27e549dcfcc2b4789d6205d973dc9eefcb
Gajanan Gaikwad <gajanangaikwad999@gmail.com>
2019-10-23 23:52:52 +0530

    commit for first screen

--- App.tsx
+++ App.tsx
@@ -2,29 +2,80 @@ import React, { Component } from 'react';
 import { Alert, Button, TextInput, View, StyleSheet, TouchableHighlight} from 'react-native';
 
 export default class App extends Component {
-  username:any
-  password:any
+  emailOrMobileNo:any
+  inp:any
   constructor(props) {
     super(props);
     
     this.state = {
-      username: '',
+      emailOrMobileNo: '',
       password: '',
     };
   }
+
+  handlePress = async () => {
+    
+    if(this.emailOrMobileNo.includes('@')){
+      this.inp="emailAddress"
+    }else{
+      this.inp="mobileNumber"
+    }
+
+  //   fetch('http://localhost:8080/users/exists?'+this.inp+'='+this.emailOrMobileNo, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //           "type": "select",
+  //     "args": {
+  //         "table": "author",
+  //         "columns": [
+  //             "name"
+  //         ],
+  //         "limit": "1"
+  //     }
+  //       })
+  // })
+  //     .then((response) => response.json())
+  //     .then((responseJson) => {
+  //  Alert.alert("Author name at 0th index:  " + responseJson[0].name);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+
+    try {
+      // let response = await fetch('http://localhost:8080/users/exists?emailAddress=gajanangaikwad999@gmail.com');
+      // Alert.alert(response.json()+ 'Login Successfull');
+      // let responseJson =  response.json;
+      // if(responseJson != undefined && null != responseJson.exists){
+      //   Alert.alert(responseJson+ 'Login Successfull');
+      // }else{
+      //   Alert.alert(responseJson+ 'Login Failed');
+      // }
+    } catch (error) {
+      Alert.alert(error);
+      console.error(error);
+    }
+
+  }
   
   onLogin() {
-    Alert.alert('Credentials', `${this.username} + ${this.password}`);
+
+
+    Alert.alert('Credentials', `${this.emailOrMobileNo}`);
   }
+
 
   render() {
     return (
       <View style={styles.container}>
         
         <TextInput
-          value={this.username}
-          onChangeText={(username) => {this.username=username}}
-          placeholder={'Email'}
+          value={this.emailOrMobileNo}
+          onChangeText={(emailOrMobileNo) => {this.emailOrMobileNo=emailOrMobileNo}}
+          placeholder={'Enter Email Or Mobile Number'}
           style={styles.input}
         />  
          <TouchableHighlight 
@@ -38,7 +89,7 @@ export default class App extends Component {
                 }}>      
         <Button
           title={'Login'}
-          onPress={this.onLogin.bind(this)}
+          onPress={this.handlePress.bind(this)}
         />
          </TouchableHighlight>  
       </View>
